6a2af106b6c52d422a35a27a4f265a1d1efe35e6
abdulaziz omonov <omonov2006omonov@gmail.com>
2025-04-20 21:39:47 +0500

    done

--- education-service/internal/repository/sms_repository.go
+++ education-service/internal/repository/sms_repository.go
@@ -17,16 +17,23 @@ func NewSmsRepository(db *sql.DB) *SmsRepository {
 
 func (r SmsRepository) GetSmsLog(companyId string, studentId string, pageReq *pb.PageRequest) (*pb.GetSmsLogResponse, error) {
 	offset := (pageReq.Page - 1) * pageReq.Size
-	baseQuery := `SELECT sms_count, array_to_string(texts, ' '), created_by_name, created_at FROM sms_used WHERE company_id = $1`
-	args := []interface{}{companyId}
 
-	if len(studentId) != 0 {
-		baseQuery += " AND student_id = $2"
-		args = append(args, studentId)
+	var (
+		baseQuery string
+		args      []interface{}
+	)
+
+	if studentId != "" {
+		baseQuery = `SELECT sms_count, array_to_string(texts, ' '), created_by_name, created_at
+		             FROM sms_used WHERE company_id = $1 AND student_id = $2
+		             ORDER BY created_at DESC LIMIT $3 OFFSET $4`
+		args = []interface{}{companyId, studentId, pageReq.Size, offset}
+	} else {
+		baseQuery = `SELECT sms_count, array_to_string(texts, ' '), created_by_name, created_at
+		             FROM sms_used WHERE company_id = $1
+		             ORDER BY created_at DESC LIMIT $2 OFFSET $3`
+		args = []interface{}{companyId, pageReq.Size, offset}
 	}
-
-	baseQuery += " ORDER BY created_at DESC LIMIT $3 OFFSET $4"
-	args = append(args, pageReq.Size, offset)
 
 	rows, err := r.db.Query(baseQuery, args...)
 	if err != nil {
