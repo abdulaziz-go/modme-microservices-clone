3f0ed14f8b7080146c3fe2659bc74068b4e06d82
abdulaziz omonov <omonov2006omonov@gmail.com>
2025-04-25 13:43:02 +0500

    done

--- education-service/internal/utils/sms.go
+++ education-service/internal/utils/sms.go
@@ -1,30 +1,85 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"github.com/realtemirov/eskizuz"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"strings"
 )
 
+var smsToken string
+
+func getEskizToken() (string, error) {
+	url := "https://notify.eskiz.uz/api/auth/login"
+	payload := strings.NewReader("email=abdulla.ergashev.2020@mail.ru&password=V16Q4KCD008jNLmgQ2zcnxxT5tgNM085BJShe17a")
+
+	req, err := http.NewRequest("POST", url, payload)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	var result map[string]interface{}
+	json.NewDecoder(res.Body).Decode(&result)
+
+	data := result["data"].(map[string]interface{})
+	token := data["token"].(string)
+
+	return token, nil
+}
+
 func SendSMS(phoneNumber, message string) error {
-	eskizClient, err := eskizuz.GetToken(&eskizuz.Auth{
-		Email:    "abdulla.ergashev.2020@mail.ru",
-		Password: "V16Q4KCD008jNLmgQ2zcnxxT5tgNM085BJShe17a",
-	})
+	if smsToken == "" {
+		token, err := getEskizToken()
+		if err != nil {
+			return err
+		}
+		smsToken = token
+	}
+
+	var b bytes.Buffer
+	w := multipart.NewWriter(&b)
+
+	w.WriteField("from", "4546")
+	w.WriteField("mobile_phone", strings.TrimPrefix(phoneNumber, "+"))
+	w.WriteField("message", message)
+	w.Close()
+
+	req, err := http.NewRequest("POST", "https://notify.eskiz.uz/api/message/sms/send", &b)
 	if err != nil {
-		return fmt.Errorf("authentication failed: %v", err)
+		return err
 	}
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.Header.Set("Authorization", "Bearer "+smsToken)
 
-	sms := &eskizuz.SMS{
-		MobilePhone: phoneNumber,
-		Message:     message,
-		From:        "4546",
-	}
-
-	result, err := eskizClient.Send(sms)
+	client := &http.Client{}
+	res, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed to send SMS: %v", err)
+		return err
+	}
+	defer res.Body.Close()
+
+	body, _ := io.ReadAll(res.Body)
+	if res.StatusCode != http.StatusOK {
+		// retry once
+		token, err := getEskizToken()
+		if err != nil {
+			return err
+		}
+		smsToken = token
+		return SendSMS(phoneNumber, message)
 	}
 
-	fmt.Printf("SMS sent successfully: %+v\n", result)
+	fmt.Println("SMS sent:", string(body))
 	return nil
 }
